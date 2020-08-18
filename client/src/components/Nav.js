d3903bed2978d59b856850a4d1680b40ec9f29ab
Andrew Jyan <andrew.jyan4@gmail.com>
2020-08-18 15:22:19 -0400

    altered build command

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -18,7 +18,7 @@ const Nav = ({ setSearchTerm, handleSearch }) => {
       </a>
 
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarNavDropdown"
@@ -26,17 +26,17 @@ const Nav = ({ setSearchTerm, handleSearch }) => {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="/">
-              Home <span class="sr-only">(current)</span>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
+          <li className="nav-item">
+            <a className="nav-link" href="/#">
               Features
             </a>
           </li>
